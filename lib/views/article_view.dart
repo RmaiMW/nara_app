657dfad880850992f36aba437a71719074aadbce
Dell <manar>
2021-04-29 21:13:38 +0300

    size

--- lib/views/article_view.dart
+++ lib/views/article_view.dart
@@ -24,7 +24,7 @@ class _ArticleViewState extends State<ArticleView> {
 
   final Completer<WebViewController> _completer =
   Completer<WebViewController>();
-  double _currentSliderValue = 250;
+
 
   bool _fav =false;
   bool _like =false;
@@ -71,13 +71,7 @@ class _ArticleViewState extends State<ArticleView> {
           centerTitle: true,
           //   backgroundColor: Colors.redAccent,
           elevation: 0.0,
-          actions: <Widget>[Builder(builder: (context) {
-            return IconButton(icon: Icon(Icons.format_size_sharp,
-              color: Colors.white,), onPressed: () {
-              Scaffold.of(context).openEndDrawer();
-            },);
-          },)
-          ],
+
           title: SingleChildScrollView(
             scrollDirection: Axis.horizontal,
             child: Row(
@@ -85,7 +79,7 @@ class _ArticleViewState extends State<ArticleView> {
               //  crossAxisAlignment: CrossAxisAlignment.end,
               children: <Widget>[
                 Row(crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
+                  children: [/*
                     FlatButton.icon(onPressed: () {},
                         icon: IconButton(icon: Icon(
                             _like ? Icons.thumb_up_alt : Icons
@@ -114,7 +108,7 @@ class _ArticleViewState extends State<ArticleView> {
                           onPressed: () async {
                             favorite();
                           },),
-                        label: Text('')),
+                        label: Text('')),*/
                     FlatButton.icon(onPressed: () {},
                         icon: IconButton(icon: Icon(
                           Icons.public, color: Colors.white,),
@@ -127,29 +121,7 @@ class _ArticleViewState extends State<ArticleView> {
             ),
           ),
         ),
-        endDrawer: Padding(
-          padding: const EdgeInsets.fromLTRB(60, 60, 20, 570),
-          child: Container( //margin: EdgeInsets.only(top: 20,left: 1,right: 40,bottom: 20),
-              color: Colors.white,
-              child: Row(mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text('Choose size of line\t',
-                    style: TextStyle(color:Theme.of(context).primaryColor,
-                        fontWeight: FontWeight.bold),),
-                  Slider(activeColor:Theme.of(context).primaryColor,
-                      value: _currentSliderValue,
-                      min: 20,
-                      max: 40,
-                      divisions: 5,
-                      label: _currentSliderValue.round().toString(),
-                      onChanged: (double s) {
-                        setState(() {
-                          _currentSliderValue = s;
-                        });
-                      })
-                ],)
-          ),
-        ),
+
         body: Container(
           height: MediaQuery
               .of(context)
@@ -187,13 +159,6 @@ class _ArticleViewState extends State<ArticleView> {
                     centerTitle: true,
                     // backgroundColor: Colors.redAccent,
                     elevation: 0.0,
-                    actions: <Widget>[Builder(builder: (context) {
-                      return IconButton(icon: Icon(Icons.format_size_sharp,
-                        color: Colors.white,), onPressed: () {
-                        Scaffold.of(context).openEndDrawer();
-                      },);
-                    },)
-                    ],
                     title: SingleChildScrollView(
                       scrollDirection: Axis.horizontal,
                       child: Row(
@@ -263,35 +228,10 @@ class _ArticleViewState extends State<ArticleView> {
                       ),
                     ),
                   ),
-                  endDrawer: Padding(
-                    padding: const EdgeInsets.fromLTRB(60, 60, 20, 570),
-                    child: Container( //margin: EdgeInsets.only(top: 20,left: 1,right: 40,bottom: 20),
-                        color: Colors.white,
-                        child: Row(mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            Text('Change size of New\t',
-                              style: TextStyle(color: Theme.of(context).primaryColor,
-                                  fontWeight: FontWeight.bold),),
-                            Slider(activeColor: Theme.of(context).primaryColor,
-                                value: _currentSliderValue,
-                                min: 200,
-                                max: 420,
-                                divisions: 30,
-                                label: _currentSliderValue.round().toString(),
-                                onChanged: (double s) {
-                                  setState(() {
-                                    _currentSliderValue = s;
-                                  });
-                                })
-
-
-                          ],)
-                    ),
-                  ),
                   body:Center(
                     child:Container(
-                      //  height:_currentSliderValue,// MediaQuery.of(context).size.height,
-                      width:_currentSliderValue,// MediaQuery.of(context).size.width,
+                      height: MediaQuery.of(context).size.height,
+                      width: MediaQuery.of(context).size.width,
                       child: WebView(
                         initialUrl: widget.blogUrl,
                         onWebViewCreated: ((WebViewController webViewController) {
