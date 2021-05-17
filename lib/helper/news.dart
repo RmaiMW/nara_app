20a3cf47d4ac0c1e482ef1f4291fb3fafd9c21fd
Dell <manar>
2021-05-17 19:53:42 +0300

    fav

--- lib/helper/news.dart
+++ lib/helper/news.dart
@@ -60,17 +60,50 @@ class News {
 
   //for saved news
   Future<void> getSavedNews(List NewsUrl) async {
-    String url ='https://newsapi.org/v2/top-headlines?country=de&category=business&apiKey=9114be959197422d932f035b9c5bc462';
-    //'http://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=3699636a771049aca38ea30dd4ac1344';
-    //https://newsapi.org/v2/top-headlines?country=de&category=business&apiKey=9114be959197422d932f035b9c5bc462
-    var response = await http.get(url);
-    var jsonData = jsonDecode(response.body);
+    var rese = await http.get("http://10.0.2.2/NARA-app/connect.php");
+    if (rese.statusCode == 200) {
+      api = json.decode(rese.body);
+      all_news.add(0);
+      for (int i = 0; i < api.length; i++) {
+        String url = api[i];
 
-    if (jsonData['status'] == 'ok') {
-      jsonData['articles'].forEach((element) {
-        NewsUrl.forEach((Urls) {
-          if (element['urlToImage'] != null && element['description'] != null && element['url']==Urls) {
-            SavedArticleModel articleSavedModel = new SavedArticleModel(
+        var response = await http.get(url);
+        var jsonData = jsonDecode(response.body);
+
+        if (jsonData['status'] == 'ok') {
+          jsonData['articles'].forEach((element) {
+            NewsUrl.forEach((Urls) {
+              if (element['urlToImage'] != null &&
+                  element['description'] != null && element['url'] == Urls) {
+                SavedArticleModel articleSavedModel = new SavedArticleModel(
+                  title: element['title'],
+                  author: element['author'],
+                  description: element['description'],
+                  url: element['url'],
+                  urlToImage: element['urlToImage'],
+                  content: element['content'],
+                );
+
+                SavedNews.add(articleSavedModel);
+              }
+            });
+          });
+        }
+      } //for
+    }
+    }
+    Future<void> getRecomNews(String category) async {
+      String url = 'https://newsapi.org/v2/top-headlines?country=de&category=$category&apiKey=9114be959197422d932f035b9c5bc462';
+      //'http://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=3699636a771049aca38ea30dd4ac1344';
+      //https://newsapi.org/v2/top-headlines?country=de&category=business&apiKey=9114be959197422d932f035b9c5bc462
+      var response = await http.get(url);
+
+      var jsonData = jsonDecode(response.body);
+
+      if (jsonData['status'] == 'ok') {
+        jsonData['articles'].forEach((element) {
+          if (element['urlToImage'] != null && element['description'] != null) {
+            ArticleModel articleModel = new ArticleModel(
               title: element['title'],
               author: element['author'],
               description: element['description'],
@@ -79,76 +112,49 @@ class News {
               content: element['content'],
             );
 
-            SavedNews.add(articleSavedModel);
+            news.add(articleModel);
           }
         });
-      });
+      }
     }
-  }
-  Future<void> getRecomNews(String category) async {
-    String url ='https://newsapi.org/v2/top-headlines?country=de&category=$category&apiKey=9114be959197422d932f035b9c5bc462';
-    //'http://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=3699636a771049aca38ea30dd4ac1344';
-    //https://newsapi.org/v2/top-headlines?country=de&category=business&apiKey=9114be959197422d932f035b9c5bc462
-    var response = await http.get(url);
+    Future<void> getHotNews() async {
+      var rese = await http.get("http://10.0.2.2/NARA-app/connect.php");
+      if (rese.statusCode == 200) {
+        api = json.decode(rese.body);
+        all_news.add(0);
+        for (int i = 0; i < api.length; i++) {
+          String url = api[i];
 
-    var jsonData = jsonDecode(response.body);
+          var response = await http.get(url);
 
-    if (jsonData['status'] == 'ok') {
-      jsonData['articles'].forEach((element) {
-        if (element['urlToImage'] != null && element['description'] != null) {
-          ArticleModel articleModel = new ArticleModel(
-            title: element['title'],
-            author: element['author'],
-            description: element['description'],
-            url: element['url'],
-            urlToImage: element['urlToImage'],
-            content: element['content'],
-          );
+          var jsonData = jsonDecode(response.body);
 
-          news.add(articleModel);
-        }
-      });
-    }
-  }
-  Future<void> getHotNews() async {
-    var rese = await http.get("http://10.0.2.2/NARA-app/connect.php");
-    if (rese.statusCode == 200) {
-      api = json.decode(rese.body);
-      all_news.add(0);
-      for(int i=0;i<api.length;i++) {
-        String url = api[i];
+          if (jsonData['status'] == 'ok') {
+            jsonData['articles'].forEach((element) {
+              if (element['urlToImage'] != null &&
+                  element['description'] != null) {
+                ArticleModel articleModel = new ArticleModel(
+                  title: element['title'],
+                  author: element['author'],
+                  description: element['description'],
+                  url: element['url'],
+                  urlToImage: element['urlToImage'],
+                  content: element['content'],
+                );
 
-        var response = await http.get(url);
-
-        var jsonData = jsonDecode(response.body);
-
-        if (jsonData['status'] == 'ok') {
-          jsonData['articles'].forEach((element) {
-            if (element['urlToImage'] != null &&
-                element['description'] != null) {
-              ArticleModel articleModel = new ArticleModel(
-                title: element['title'],
-                author: element['author'],
-                description: element['description'],
-                url: element['url'],
-                urlToImage: element['urlToImage'],
-                content: element['content'],
-              );
-
-              agnews.add(articleModel);
-            }
-          });
-        }
-        all_news.add(agnews.length);
-      }//for
-    }
-    for(int i=0;i<all_news.length-1;i++){
-      news.add(agnews[all_news[i]]);
-
+                agnews.add(articleModel);
+              }
+            });
+          }
+          all_news.add(agnews.length);
+        } //for
+      }
+      for (int i = 0; i < all_news.length - 1; i++) {
+        news.add(agnews[all_news[i]]);
+      }
     }
   }
 
-}
 
 class CategoryNewsClass {
   List<ArticleModel> news = [];
